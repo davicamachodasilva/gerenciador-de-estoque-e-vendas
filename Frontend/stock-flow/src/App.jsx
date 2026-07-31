1add773877421a98f4be2479e3eacd590e54b616
Davi Camacho <davicamachodasilva@gmail.com>
2026-07-31 17:28:45 -0300

    Atualização nas estilisações do app.jsx index.css sidebar.modelue.css para o sidebar permanecer na esquerda independente da barra d erolagem esta embaixo ou emcima ou seja o sidebar não vai ser afetado por mudanças do layout dos outros componentes

--- Frontend/stock-flow/src/App.jsx
+++ Frontend/stock-flow/src/App.jsx
@@ -1,16 +1,14 @@
 import Sidebar from "./components/sidebar"
 import Produtos from "./pages/Produtos"
 
-function App(){
-  return(
-      <div style={{display:'flex'}}>
-        <Sidebar/>
-        <main style={{padding:'20px', flex:1}}>
-          <Produtos/>
-        </main>
-      </div>
-
-
+function App() {
+  return (
+    <div style={{ display: 'flex', minHeight: '100vh' }}>
+      <Sidebar />
+      <main style={{ padding: '20px', flex: 1, minWidth: 0 }}>
+        <Produtos />
+      </main>
+    </div>
   )
 }
 
